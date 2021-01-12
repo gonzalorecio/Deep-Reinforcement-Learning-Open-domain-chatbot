3b83b780ec6fa4f090919f3988a0bf928267ce61
jreventos <janareventos13@gmail.com>
2021-01-12 23:09:40 +0100

    update interface

--- main2.py
+++ main2.py
@@ -1,6 +1,4 @@
 from transformers import  BertModel, BertTokenizer
-import torch
-import torch.nn as nn
 import requests
 from sentiments.BERTGRU_model import BERTGRUSentiment
 
@@ -23,7 +21,7 @@ class ChatbotFace:
         self.change_status(text)
 
     def predict_sentiment(self, sentence):
-        model_path = '/Users/jreventos/Desktop/MAI/Semester 3/CIR/MAI-CIR/BERT_sentiment.pt'
+        model_path = 'BERT_sentiment.pt'
         max_input_length =  512
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -59,27 +57,27 @@ class ChatbotFace:
         return prediction.item()
 
     def mood_prediction(self, utterance):
-        #self.change_status('thinking')
-        #self.change_mood('start_blinking')
+
         score = self.predict_sentiment(utterance)
 
-        # print(score)
         self.status_custom(utterance)
-        if '?' not in utterance:
+
+        if 'how' or 'what' or 'why' or 'which' or 'when' in utterance:
+            self.change_mood('confused')
+        else:
             if score <= 0.55 and score > 0.45:
                 self.change_mood('neutral')
             elif score <= 1 and score > 0.55:
                 self.change_mood('happy')
             elif score <= 0.45 and score > 0:
                 self.change_mood('sad')
-        else:
-            self.change_mood('confused')
+
 
     def start_blinking(self):
         self.change_mood('start_blinking')
 
     def stop_blinking(self):
-        self.change_mood('stopt_blinking')
+        self.change_mood('stop_blinking')
 
     def status_listening(self):
         self.change_status('listening')
@@ -90,50 +88,138 @@ class ChatbotFace:
     def status_none(self):
         self.change_status('')
 
-    def neutral(self):
-        self.change_mood('neutral')
 
-    def happy(self):
-        self.change_mood('happy')
 
-    def sad(self):
-        self.change_mood('sad')
-
-    def angry(self):
-        self.change_mood('angry')
-
-    def focused(self):
-        self.change_mood('focused')
-
-    def confused(self):
-        self.change_mood('confused')
 
 
 
 # SPEECH
 from speech import google_speech as speech
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelWithLMHead
 from google_trans_new import google_translator
-
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+import webbrowser as web
 
 class Chatbot:
     def __init__(self, lang='en'):
+
+
+        web.open("https://gonzalorecio.com/chatbot/robot.html")
+
         print('Loading model and tokenizers...')
+
+        # Initialize chatbot interface
         self.face = ChatbotFace()
-        self.face.status_custom('Welcome to our chatbot interface! I will be ready in a few seconds.')
+
+        # Define chatbot voice
+        self.define_voice(lang=lang)
+
+        # Load model
         self.model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
         self.tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+        #self.tokenizer = AutoTokenizer.from_pretrained("ncoop57/DiGPTame-medium")
+        #self.model = AutoModelWithLMHead.from_pretrained("ncoop57/DiGPTame-medium")
+
+        # Display instructions:
+        self.initial_instructions(lang=lang)
+
+        # Reset chatbot
         self.reset_chatbot(lang=lang)
-        self.speak('Welcome to our chatbot interface. In a few seconds you can start a conversation with me. Now I am loading the system, I will be ready in a second!')
+        #self.speak('Welcome to our chatbot interface. In a few seconds you can start a conversation with me. Now I am loading the system, I will be ready in a second!')
+
+        # Welcome message
+        if lang == 'en':
+            self.face.status_custom('Hey I am CIRREL bot!')
+            self.speak('Hey my name is CIRREL, and I am a chatbot developed by Gonzalo Recio and Jana Reventós.   '
+                       'In a few seconds you can start a conversation with me.   '
+                       'In the blink of an eye, I will be ready.')
+        else:
+            self.face.status_custom('Hola, me llamo CIRREL!')
+            self.speak(
+                'Hola mi nombre es CIRREL, y soy un chatbot creado por Gonzalo Recio y Jana Reventós.   '
+                'En unos segundos podrás empezar una conversación conmigo.   '
+                'Estaré preparado en menos de un abrir y cerrar de ojos!')
+
         print('Chatbot ready.')
 
-    def reset_chatbot(self, lang='en'):
-        self.chat_history_ids = self.tokenizer.encode(self.tokenizer.bos_token, return_tensors='pt')
+
+    def initial_instructions(self,lang='en'):
+
+        if lang == 'en':
+            text1 = 'Please read the following instructions (1-7) before starting the conversation:'
+        else:
+            text1 = 'Porfavor lea las siguientes instrucciones (1-7) antes de empezar la conversación:'
+        self.face.status_custom(text1)
+        self.speak(text1)
+
+        # Instruction 1
+        if lang == 'en':
+            inst1  = '1. This is a test for our CIR subject final project.'
+        else:
+            inst1 = '1. Esto es un test para nuestro proyecto de la asignatura de CIR.'
+        self.face.status_custom(inst1)
+        self.speak(inst1)
+
+        # Instruction 2
+        if lang == 'en':
+            inst2 = '2. We aim to test this conversational system with real users.'
+        else:
+            inst2 = '2. Nuestro objetivo es realizar una prueba de nuestro sistema de conversación con usuarios reales.'
+
+        self.face.status_custom(inst2)
+        self.speak(inst2)
+
+        # Instruction 3
+        if lang == 'en':
+            inst3 = '3. This session will be recorded to test the reaction of the users during the interaction with the chatbot.'
+        else:
+            inst3 = '3. Esta sesión será grabada para evaluar la reacción de los usuarios durante la interacción con el chatbot.'
+
+        self.face.status_custom(inst3)
+        self.speak(inst3)
+
+        # Instruction 4
+        if lang == 'en':
+            inst4 = '4. When you read the phrase: I AM LISTENING in the screen, you have to speak.'
+        else:
+            inst4 = '4. Cuando leas la frase: I AM LISTENING en la pantalla, deberás hablar.'
+        self.face.status_custom(inst4)
+        self.speak(inst4)
+
+        # Instruction 5
+        if lang == 'en':
+            inst5 = '5. Please wait while the chatbot is thinking an answer. You will read the word THINKING in the screen.'
+        else:
+            inst5 = '5. Porfavor, espera a que el chatbot pinse su respuesta. Vas a leer la palabra THINKING en la pantalla.'
+        self.face.status_custom(inst5)
+        self.speak(inst5)
+
+        # Instruction 6
+        if lang == 'en':
+            inst6 = '6. Once the answer is ready, the chatbot will talk to you.'
+        else:
+            inst6 = '6. Una vez la respuesta este preparada, el chatbot te va a contestar.'
+        self.face.status_custom(inst6)
+        self.speak(inst6)
+
+        # Instruction 7
+        if lang == 'en':
+            inst7 = '7. Then, you can answer the chatbot again.'
+        else:
+            inst7 = '7. Cuando hayas escuchado la respuesta podrás volver a responder al chatbot.'
+        self.face.status_custom(inst7)
+        self.speak(inst7)
+
+    def define_voice(self,lang='en'):
         if lang == 'es':
             self.voice = 14  # 0: spanish female, 1: english female, 2: english male
         else:
             self.voice = 0  # 0: spanish female, 1: english female, 2: english male
+
+    def reset_chatbot(self, lang='en'):
+        self.chat_history_ids = self.tokenizer.encode(self.tokenizer.bos_token, return_tensors='pt')
         self.translator = google_translator()
         self.lang = lang
 
@@ -141,6 +227,7 @@ class Chatbot:
         self.face.status_listening()
         lang = 'en-US' if self.lang == 'en' else 'es-ES'
         question = speech.speech_to_audio()
+        self.question_original = question
         if self.lang == 'es':
             question = self.translate(question, lang='en')
         self.face.status_none()
@@ -166,9 +253,9 @@ class Chatbot:
         return True
 
     def speak(self, text):
-        self.face.happy()
+        self.face.change_mood('happy')
         speech.text_to_speech(text, speed = 200, vol=1.0, voice=self.voice)
-        self.face.neutral()
+        self.face.change_mood('neutral')
 
     def translate(self, text, lang):
         translated_text = self.translator.translate(text, lang_tgt=lang)
@@ -179,12 +266,12 @@ class Chatbot:
     def no_understand(self):
         text = "Sorry, I couldn't understand you"
         if self.lang == 'es':
-            text = self.translete(text, lang='es')
+            text = self.translate(text, lang='es')
         self.speak(text)
 
     def run_chat(self):
 
-        self.face.neutral()
+        self.face.change_mood('neutral')
         # Chatbot welcome
         self.reset_chatbot(lang=self.lang)
         question = ''
@@ -195,25 +282,31 @@ class Chatbot:
                 print('Fallo')
                 self.no_understand()
                 continue
-            print('Question:', question)
+            print('User:', self.question_original)
             answer = self.generate_answer(question)
-
+            self.face.mood_prediction(answer)
+            self.face.status_custom(answer)
+            print('CIRREL chatbot:', answer)
+            self.speak(answer)
             if answer != previous_answer:
-                self.face.mood_prediction(answer)
-                self.face.status_custom(answer)
-                print('Answer:', answer)
-                self.speak(answer)
                 previous_answer = answer
+                self.chat_history_ids = self.chat_history_ids[:, -50:]
                 continue
             else:
-                self.face.status_custom('This conversation has ended')
-                self.speak('This conversation has ended.')
-                print('Conversation ended')
+
+                if self.lang == 'en':
+                    self.face.status_custom('This conversation has ended')
+                    self.speak('This conversation has ended. It has been a pleasure talking with you. Thank you! ')
+                    print('Conversation ended')
+                else:
+                    self.face.status_custom('Esta conversación ha terminado.')
+                    self.speak('Esta conversación ha terminado. Ha sido un placer hablar contigo. Muchas gracias!')
+                    print('Conversación terminada.')
                 break
-
-
+        self.face.status_none()
             # self.reset_chatbot(lang=self.lang)
             # print(self.chat_history_ids)
+
 
 chatbot = Chatbot(lang='es')
 chatbot.run_chat()
